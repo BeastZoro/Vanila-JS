c64418d80014545e5168173d04f1616006d0329b
BeastZoro <126886437+BeastZoro@users.noreply.github.com>
2023-12-07 18:44:13 +0530

    tabs changes added

--- tabs/app.js
+++ tabs/app.js
@@ -1,17 +1,16 @@
-const tabs = document.querySelectorAll('.btn')
-const tabText = document.querySelectorAll('.text')
+const tabs = document.querySelectorAll('.text')
 const article = document.querySelector('article')
+const buttons = document.querySelectorAll('.btn')
 
+article.addEventListener('click', (e) => {
+    const tabId = e.target.dataset.id
 
-article.addEventListener('click', (e) =>{
-    const dataId = e.target.dataset.id
-    if(dataId){
-        tabs.forEach(tab => tab.classList.remove('active'))
+    if (tabId) {
+        buttons.forEach((button) => button.classList.remove('active'))
         e.target.classList.add('active')
 
-        tabText.forEach(tab =>tab.classList.remove('active'))
-        const clickedTab = document.getElementById(dataId)
-        clickedTab.classList.add('active')
+        tabs.forEach((tab) => tab.classList.remove('active'))
+        const activeTab = document.getElementById(tabId)
+        activeTab.classList.add('active')
     }
-
 })
