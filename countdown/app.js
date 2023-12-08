a40557e668c837a97a4ef4690e2aa2bf640df484
BeastZoro <126886437+BeastZoro@users.noreply.github.com>
2023-12-08 18:21:57 +0530

    style completed

---
+++ countdown/app.js
@@ -0,0 +1,8 @@
+const days = document.getElementById('days')
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
+
+
+const currentData = new Date()
+console.log(currentData)
